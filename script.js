04dbde607bb20f44addd9d2bf2f97fddd39f3a93
Scott Donaldson <scott.p.donaldson@gmail.com>
2016-08-20 13:39:31 -0400

    home page filter menu reordering

---
+++ script.js
@@ -0,0 +1,26 @@
+(function($) {
+
+	var filterMenu = $('body.home .menu-smart').not('#menu-nav');
+	var order = ['Design Research', 'Service Design', 'UX', 'Experimental'];
+	var index = 0;
+	var ordered = false;
+	(function reorder() {
+
+		var filterMenu = $('body.home .menu-smart').not('#menu-nav');
+		if ( filterMenu.length === 0 ) return setTimeout(reorder, 10);	
+
+		var test = order[index],
+			current = filterMenu.children().eq(index + 1),
+			next = current.next();
+
+		if ( current.find('a').text() !== test ) {
+			// must come later...
+			next.insertBefore(current);
+		} else {
+			if ( index === order.length - 1 ) return;
+			index++;
+		}
+
+		reorder();
+	})();
+})(jQuery);
